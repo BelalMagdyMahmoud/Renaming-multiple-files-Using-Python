3bbc0b9724661cfcc67f9ebee1c7d4f1e29ea1e1
BelalMagdyMahmoud <97229369+BelalMagdyMahmoud@users.noreply.github.com>
2022-09-14 20:20:50 +0200

    Update renaming multiple files.py

--- renaming multiple files.py
+++ renaming multiple files.py
@@ -1,14 +1,14 @@
 import os
 
-for i in range(0,200,1):
+for i in range(0,201,1):
 # Absolute path of a file
-    old_name = r"D:\Belal\ITI\Python\New folder\gaag "+str(i)+".txt"
-    new_name = r"D:\Belal\ITI\Python\New folder\gag "+str(i)+".txt"
+    old_name = r"D:\Belal\ITI\Python\New folder\gag "+str(i)+".txt"
+    new_name = r"D:\Belal\ITI\Python\New folder\gaag "+str(i)+".txt"
 
 # Renaming the file
     try:
         os.rename(old_name, new_name)
     except WindowsError:
-        print (old_name)
+        print (new_name)
 
 print ("@BelalMagdyMahmoud")
